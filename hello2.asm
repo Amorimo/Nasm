29c5e9781efa31363a1078018c9318cf1d679d5a
Matheus Amorim <matheus.a.amorim10@gmail.com>
2024-10-18 16:58:35 -0300

    Criação de outra mensagem

---
+++ hello2.asm
@@ -0,0 +1,14 @@
+section .data
+    msg:db 'Olá progamadores',10
+section .text
+    global _start
+
+_start:
+    mov rax,1
+    mov rdi,1
+    mov rsi,msg
+    mov rdx,18
+    syscall
+    mov rax,60
+    xor rdi, rdi
+    syscall
